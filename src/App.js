5e88bbbf03353c1fc7a34351da27dfdb6fbd780d
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-11-06 17:11:35 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+
+import React, { useState } from 'react';
+import About from "./components/About";
+import Increment from "./components/Increment";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Clock from "./components/Clock"
+import Alert from "./components/Alert"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // eslint-disable-next-line
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert= (message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    })
+  }
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils-Dark mode"
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success")
+      document.title = "TextUtils-Light mode"
+    }
+  }
+  return (
+    <>
+    <Router>
+      <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+        </Route>
+      </Switch>
+      <div className="container">
+        <Increment heading="Click here" />
+      </div>
+      <Clock mode={mode}/>
+      </Router>
+    </>
+  );
+}
+
+export default App;
