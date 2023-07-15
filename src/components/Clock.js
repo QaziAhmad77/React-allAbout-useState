04e88a54875319ec00000e20501f8520f2f8ef34
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-07-15 14:11:51 +0500

    Update Clock.js

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,16 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 export default function Clock(props) {
-  let time = new Date().toLocaleTimeString();
-  const [ctime, setCtime] = useState(time)
-  const UpdateTime = ()=>{
-   time = new Date().toLocaleTimeString()
-  setCtime(time);
-  }
-  setInterval(UpdateTime,1000)
+  let time = new Date();
+  console.log(time, 'this is time');
+  const [ctime, setCtime] = useState(time);
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(UpdateTime, 1000);
   return (
     <>
-    <h1 style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh', backgroundColor: props.mode==="dark"?"black":"white", margin: "0px", color: props.mode==="dark"?"white":"black", fontSize: "50px"}}> {ctime} </h1>
+      <h1
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          backgroundColor: props.mode === 'dark' ? 'black' : 'white',
+          margin: '0px',
+          color: props.mode === 'dark' ? 'white' : 'black',
+          fontSize: '50px',
+        }}
+      >
+        {' '}
+        {ctime}{' '}
+      </h1>
     </>
-  )
+  );
 }
